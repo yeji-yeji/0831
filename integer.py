c93d1c5e869d7b695ee839bf7afbc868ea3e898e
yeji-yeji <54461231+yeji-yeji@users.noreply.github.com>
2019-09-21 12:47:10 +0900

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,7 @@
+print(1)
+x=9
+print("{0}".format(x))
+print("{}".format(3**4))
+print("{}".format(int(9.3)/int(2.7)))
+
+
---
+++ string.py
@@ -0,0 +1,9 @@
+print("{:s}".format('I\'m enjoying learning Python.'))
+
+print("{0:s}".format("This is a long string Without the backslash\
+it would run off of the page on the right in the text editor and be very\
+difficult in read and edit. By usning the backslash you can split the long\
+string into smaller strings on seperate lines so that the whole string is easy\
+to view on the editor."))
+
+print ("{0:s}".format("""You can use triple single quotes for multi-line comment strings."""))
